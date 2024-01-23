097286b00d5d47b0ce300751ddd6ebb50882267c
Caty Laporta <catalinalaporta@gmail.com>
2024-01-23 16:58:23 -0300

    Arreglando estilos e imagenes

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const path = require(`path`);
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, `./public`);
-app.use(express.static(publicPath));
+app.use(express.static('public'));
 
 const port=process.env.PORT || 3000;
 
