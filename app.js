6af235cbd38cc3547794792cb4bc62fb1db0df5e
Caty Laporta <catalinalaporta@gmail.com>
2024-06-08 11:31:37 -0300

    creacion del formulario de registro y modificacion de archivos asociados

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require(`express`);
 const path = require(`path`);
-
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port=process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en puerto 3000`);
+    console.log(`Servidor corriendo en puerto ${port}`);
 });
 
 
 app.get(`/`, (req, res) => {
     res.sendFile(path.resolve(__dirname, `./views/home.html`));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+app.post('/register', (req, res) => {
+    // Por ahora, solo redirigiremos a la página principal
+    console.log(req.body); // Imprimir datos del formulario en la consola para fines didácticos
+    res.redirect('/');
 });
